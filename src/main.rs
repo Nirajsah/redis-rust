2bebf4ea48d3f78f2572935a14869a8561f7537c
Nirajsah <nirajsahy4aj@gmail.com>
2024-04-11 15:06:19 +0530

    GET SET

--- src/main.rs
+++ src/main.rs
@@ -28,13 +28,16 @@ fn parser(data: &str) -> Vec<&str> {
         let k: Vec<&str> = s[i].split("\r\n").collect();
         v.push(k[1]);
     }
-
     v
+}
+
+struct Bar {
+    foo: String,
 }
 
 fn handle_connection(mut stream: TcpStream) {
     let mut buf = [0; 1024];
-
+    let mut ata = Bar { foo: String::new() };
     loop {
         let data = stream.read(&mut buf).unwrap();
         if data == 0 {
@@ -42,12 +45,24 @@ fn handle_connection(mut stream: TcpStream) {
         }
         let y = String::from_utf8_lossy(&buf[..data]);
         let x = parser(&y);
+
         if x.len() == 0 {
             break;
         } else if x[0] == "echo" {
             let _ = stream
                 .write(format!("${}\r\n{}\r\n", x[1].len(), x[1]).as_bytes())
                 .unwrap();
+        } else if x[0] == "set" {
+            ata.foo = x[2].to_string();
+            let _ = stream.write(b"+OK\r\n").unwrap();
+        } else if x[0] == "get" {
+            if x[1] != "foo" {
+                let _ = stream.write(b"$-1\r\n").unwrap();
+            } else {
+                let _ = stream
+                    .write(format!("${}\r\n{}\r\n", ata.foo.len(), ata.foo).as_bytes())
+                    .unwrap();
+            }
         } else {
             stream.write(b"+PONG\r\n").unwrap();
         }
