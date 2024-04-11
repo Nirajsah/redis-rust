cd0e4c6114d48d80a7bab8f47036712434b9f498
Nirajsah <nirajsahy4aj@gmail.com>
2024-04-11 14:46:28 +0530

    echo command

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+#![allow(unused_imports)]
 use std::{
     io::{Read, Write},
     net::{TcpListener, TcpStream},
@@ -5,6 +6,7 @@ use std::{
 
 fn main() {
     println!("Logs from your program will appear here!");
+
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
 
     for stream in listener.incoming() {
@@ -19,6 +21,16 @@ fn main() {
         }
     }
 }
+fn parser(data: &str) -> Vec<&str> {
+    let s: Vec<&str> = data.split("$").collect();
+    let mut v: Vec<&str> = [].to_vec();
+    for i in 1..s.len() {
+        let k: Vec<&str> = s[i].split("\r\n").collect();
+        v.push(k[1]);
+    }
+
+    v
+}
 
 fn handle_connection(mut stream: TcpStream) {
     let mut buf = [0; 1024];
@@ -28,6 +40,17 @@ fn handle_connection(mut stream: TcpStream) {
         if data == 0 {
             break;
         }
-        stream.write(b"+PONG\r\n").unwrap();
+        let y = String::from_utf8_lossy(&buf[..data]);
+        let x = parser(&y);
+        if x.len() == 0 {
+            break;
+        } else if x[0] == "echo" {
+            let _ = stream
+                .write(format!("${}\r\n{}\r\n", x[1].len(), x[1]).as_bytes())
+                .unwrap();
+        } else {
+            stream.write(b"+PONG\r\n").unwrap();
+        }
+        println!("{:?}", x);
     }
 }
